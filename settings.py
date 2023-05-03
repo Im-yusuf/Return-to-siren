8d9ce75648e99d2d75fb0ec4b4e06e7d18815b12
Im-yusuf <132395776+Im-yusuf@users.noreply.github.com>
2023-05-03 11:03:26 +0100

    update

---
+++ settings.py
@@ -0,0 +1,43 @@
+map_layout = [
+    'X                                           ',
+    'X                                           ',
+    'X     C           XX      XX      XX        ',
+    'XC    XX         X  X    X  X    X  X       ',
+    'XX    XX         B              C           ',
+    'X    CXX                      CC CC   G     ',
+    'X    XXX  P   CS    S     S                 ',
+    'XC     XXXXX  XX  XXXX    XX          X     ',
+    'XX    CCXX     X  CXX    XXH X     S XXXX   ',
+    'XXX   XXX       XXXX         XX   XXX       ',
+    'XXXX       X    X XXC  X  CCCXX CXXXX       ',
+    'XXXXXXXXXXXX  XXXXXXXX XXXXXX  XX  XXXX     ',]
+
+map_layout2 = [
+    'X                  XX      XX                 XX       XX    ',
+    'X      CC         XXXX    XXXX               XXXX     XXXX   ',
+    'X   C S                          C                           ',
+    'X   XXXXXX                       XX                    G     ',
+    'XC      XX                      XX         CC                ',
+    'XX     CXX  S  X   S         S            C  C       XXXX    ',
+    'X      XXX XXX XC XXX   X  XXXXX         X        X   XX     ',
+    'XP  XX  XX  X  XX XXX  XX   XXX      S           XX   XX     ',
+    'XXXXXX  XX  X  XC  CX XXX  CXXX     XXX   S  S  XXX   XX     ',
+    'XXXXXX  XX  X  XXXXXXXXXX  XXXX     XXX XXXXXXXXXXX   XX     ',
+    'XXXXXX  XX  X  XXXXXXXXXX   XXX     XXX XXXXXXXXXXX   XX     ',]
+levels = {1: map_layout, 2: map_layout2}
+
+tile_size = 64
+screen_width = 1470
+screen_height =len(map_layout) * tile_size
+map_layout3=[
+  '   XP   ',
+  '  XXX',
+  ' XXXXX',
+  'XXXXXXX',]
+# letters = ["a", "b", "c"]
+
+# p1 = random.choice(letters)
+# p2 = random.choice(letters)
+# p3 = random.choice(letters)
+
+# string = f"{p1} jfksdjfdsk {p2} jskljksd {p3} fjlia"
